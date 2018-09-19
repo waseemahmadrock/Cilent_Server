acd831e065e51eae6dac5167ca7f8d5d2232d087
Waseem Akhtar <waseemahmadrock@gmail.com>
2018-09-19 13:28:36 +0500

    Add files via upload

---
+++ ClientExample/Program.cs
@@ -0,0 +1,51 @@
+﻿
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+namespace ClientExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket dailer = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            //Socket listner = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Socket Created");
+
+            IPAddress ipAddress = IPAddress.Loopback;
+            //IPAddress ipAddress=IPAddress.Parse("127.0.0.1");
+            Console.WriteLine("IP Assigned");
+
+            IPEndPoint ipendpoint = new IPEndPoint(ipAddress, 8001);
+            Console.WriteLine("Endpint Ready");
+           
+            try
+            {
+                dailer.Connect(ipendpoint);
+                Console.WriteLine("Connected To Server");
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Unable to Connect");
+            }
+
+
+            NetworkStream ns = new NetworkStream(dailer);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+
+            //Communication
+            sw.WriteLine();
+            sw.Flush();
+
+            sr.ReadLine();
+
+            dailer.Close();
+            Console.WriteLine("Closed!");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SeverExample/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace SeverExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket listner = new Socket(SocketType.Stream, ProtocolType.Tcp);
+            //Socket listner = new Socket(AddressFamily.InterNetwork,SocketType.Stream, ProtocolType.Tcp);
+            Console.WriteLine("Socket Created");
+
+            IPAddress ipAddress = IPAddress.Loopback;
+            //IPAddress ipAddress=IPAddress.Parse("127.0.0.1");
+            Console.WriteLine("IP Assigned");
+
+            IPEndPoint ipendpoint = new IPEndPoint(ipAddress, 8001);
+            Console.WriteLine("Endpint Ready");
+            
+            listner.Bind(ipendpoint);
+            Console.WriteLine("Socket Bind");
+            
+            listner.Listen(1);
+            Console.WriteLine("Now Listening...");
+
+            Socket client=listner.Accept();
+            Console.WriteLine("Client Connected");
+
+            NetworkStream ns = new NetworkStream(client);
+            StreamReader sr = new StreamReader(ns);
+            StreamWriter sw = new StreamWriter(ns);
+
+            //Communication
+            sr.ReadLine();
+
+            sw.WriteLine();
+            sw.Flush();
+
+            listner.Close();
+            Console.WriteLine("Closed!");
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
